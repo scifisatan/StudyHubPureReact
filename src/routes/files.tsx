d3988482933b997b4f8aebf52a3a997338c6d9bc
scifisatan <brawlershere@gmail.com>
2024-09-18 23:15:59 +0545

    feat: add pdf preview and ux improvement

--- src/routes/files.tsx
+++ src/routes/files.tsx
@@ -3,16 +3,22 @@ import { getPDFSummary } from "@/api";
 import { ChatSideBar } from "@/components/chatsidebar";
 import { Dropzone } from "@/components/dropzone";
 import { Notes } from "@/components/notes";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { PrivateRoute } from "@/routes/private";
-import { Loader2 } from "lucide-react";
+import { Loader, Loader2 } from "lucide-react";
 import { toast } from "sonner";
 
 function FilesPage() {
   const [markdownContent, setMarkdownContent] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleFileChange = async (file: File) => {
+  const handleFileChange = (file: File) => {
+    setFile(file);
+  };
+
+  const submitFile = async (file: File) => {
     setIsLoading(true);
     try {
       const data = await getPDFSummary(file);
@@ -31,16 +37,33 @@ function FilesPage() {
           <CardTitle className="text-2xl font-bold">PDF Summarizer</CardTitle>
         </CardHeader>
         <CardContent className="space-y-6">
+          {file && (
+            //preview of pdf file
+            <div className="flex items-center justify-center p-4">
+              <iframe
+                src={URL.createObjectURL(file)}
+                className="h-96 w-full rounded-lg border-2 border-gray-300 shadow-lg"
+              />
+            </div>
+          )}
           <Dropzone onFileChange={handleFileChange} />
+          <div className="flex justify-end">
+            <Button
+              onClick={() => file && submitFile(file)}
+              disabled={isLoading || !file}
+            >
+              {isLoading ? (
+                <span className="flex items-center">
+                  <Loader2 className="mr-2 animate-spin" size={24} />
+                  Generating Summary...
+                </span>
+              ) : (
+                "Submit"
+              )}
+            </Button>
+          </div>
         </CardContent>
       </Card>
-
-      {isLoading && (
-        <div className="flex items-center justify-center">
-          <Loader2 className="mr-2 h-8 w-8 animate-spin" />
-          <span>Processing the file...</span>
-        </div>
-      )}
 
       {markdownContent != "" && <Notes notes={markdownContent} />}
       <ChatSideBar context={markdownContent} />
